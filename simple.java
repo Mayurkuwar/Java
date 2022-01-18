41281422ca19358643b9550a2a48700e2d841f61
Mayurkuwar <97946442+Mayurkuwar@users.noreply.github.com>
2022-01-18 22:16:50 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,7 +1,4 @@
-class simple{
-public statis void main(String args[]){
-
-
-system.out.println("welcome to the java");
-}
-}
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
